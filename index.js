85cb51d7cbd43ee12a9629fd21062b48204b77ef
guillenjs <jorgeguilln@gmail.com>
2020-12-17 15:35:03 -0500

    styling update

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ let footerImage = document.createElement('img')
     footerImage.src = 'images/pattern-hills.svg'
     footer.append(footerImage)
 
+
 const boxes = () => {
 
 //first box
+    let outer = document.createElement('div')
+        outer.className = 'outer'
+
     let box = document.createElement('div')
+    let label = document.createElement('p')
+        label.innerText = 'Day'
+
+    outer.append(box, label)
     box.className = 'box'
-    timer.append(box)
+ 
+    timer.append(outer)
     // box.innerText = "1"
 
     let cornerCut = document.createElement('div')
@@ -27,10 +36,16 @@ const boxes = () => {
     box.append(cornerCut, textDiv, cornerCut2)
 
 //second box
+let outer2 = document.createElement('div')
+        outer2.className = 'outer'
 
     let box2 = document.createElement('div')
+    let label2 = document.createElement('p')
+        label2.innerText = 'Hours'
+
     box2.className = 'box'
-    timer.append(box2)
+    outer2.append(box2, label2)
+    timer.append(outer2)
 
     let cornerCut3 = document.createElement('div')
     cornerCut3.className = 'cornerCut'
@@ -44,9 +59,16 @@ const boxes = () => {
     box2.append(cornerCut3, textDiv2, cornerCut4)
 //third box
 
+let outer3 = document.createElement('div')
+        outer3.className = 'outer'
+
     let box3 = document.createElement('div')
+    let label3 = document.createElement('p')
+        label3.innerText = 'Minutes'
+    outer3.append(box3, label3)
+
     box3.className = 'box'
-    timer.append(box3)
+    timer.append(outer3)
 
     let cornerCut5 = document.createElement('div')
     cornerCut5.className = 'cornerCut'
@@ -60,9 +82,15 @@ const boxes = () => {
     box3.append(cornerCut5, textDiv3, cornerCut6)
 
 //fourth box
+let outer4= document.createElement('div')
+        outer4.className = 'outer'
+
     let box4 = document.createElement('div')
     box4.className = 'box'
-    timer.append(box4)
+    let label4 = document.createElement('p')
+        label4.innerText = 'Hours'
+        outer4.append(box4, label4)
+    timer.append(outer4)
 
     let cornerCut7 = document.createElement('div')
     cornerCut7.className = 'cornerCut'
@@ -76,6 +104,8 @@ const boxes = () => {
     box4.append(cornerCut7, textDiv4, cornerCut8)
 
 }
+
+
 
 boxes()
 
